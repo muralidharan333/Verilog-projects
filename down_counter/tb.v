d383aa7bb45884e36b9ff16d7b8069c0fdf7fb7e
Murali Dharan <muralidharan13189@gmail.com>
2026-09-25 10:36:57 +0530

    Add testbench for down_counter module

---
+++ down_counter/tb.v
@@ -0,0 +1,25 @@
+module down_counter_tb;
+	reg clk=1'b0,reset;
+	wire [3:0]count;
+	
+	down_counter DUT(clk,reset,count);
+	always #5 clk = ~clk;
+	
+	task rst;
+	begin
+		@(negedge clk);
+		reset=1'b1;
+		@(negedge clk);
+		reset=1'b0;
+	end
+	endtask
+	
+	initial begin
+		$dumpfile("down_counter.vcd");
+		$dumpvars(0,down_counter_tb);
+		rst;
+		repeat(20) @(posedge clk);
+		$finish;
+	end
+initial $monitor("time=%t,clk=%b,reset=%b,count=%b",$time,clk,reset,count);
+endmodule
